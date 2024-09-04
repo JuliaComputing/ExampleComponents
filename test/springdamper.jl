1ca371dfee39ce85ae10705cea0c1f6befafaa5c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-09-03 20:43:15 -0400

    better plot

--- test/springdamper.jl
+++ test/springdamper.jl
@@ -2,8 +2,9 @@ using ExampleComponents
 using OrdinaryDiffEq, ModelingToolkit
 
 @mtkbuild springdamper = SpringDamper()
-prob = ODEProblem(springdamper, [springdamper.x => 1.0], (0.0, 1.0))
+prob = ODEProblem(springdamper, [springdamper.x => 2.0], (0.0, 12.0))
 sol = solve(prob)
 
 using Plots
 plot(sol)
+savefig("plot.png")
